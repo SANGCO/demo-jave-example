42793eb66e2d2d1204a4334a0c92d021bc00e7b3
sangconam <sangconam@nexybit.com>
2019-07-29 08:46:47 +0900

    first commit

---
+++ src/main/java/com/example/demojavaexample/DemoJavaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojavaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJavaExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demojavaexample/DemoJavaExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demojavaexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoJavaExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
